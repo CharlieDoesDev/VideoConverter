fa43de21d64a22287fc0b85ce8dc65feb6061b2b
simonswartout <121693487+simonswartout@users.noreply.github.com>
2025-05-12 17:41:40 -0400

    Fixed Terser Problem

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,19 @@
 // rollup.config.js
-import resolve from '@rollup/plugin-node-resolve';
+import resolve  from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import terser   from '@rollup/plugin-terser';  // default import, not destructured
 
 export default {
   input: 'js/converter.js',
   output: {
-    file: 'js/vendor/bundle.js',
-    format: 'iife',        // immediately-invoked for browsers
-    name: 'VideoConverter',
-    plugins: [terser()]
+    file:      'js/vendor/bundle.js',
+    format:    'iife',        // immediately‐invoked for browsers
+    name:      'VideoConverter',
+    sourcemap: true,          // generate source maps
+    plugins:   [ terser() ]   // minify via terser()
   },
   plugins: [
-    resolve(),            // locate and bundle mp4box & webm-muxer
-    commonjs()            // convert CommonJS modules to ES
+    resolve(),   // bundle MP4Box & webm-muxer
+    commonjs()   // convert any CommonJS modules
   ]
 };
